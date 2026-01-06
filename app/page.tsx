52019893ac7e6bed6090297b20804a814f9cf5b7
JuwelRana34 <juwelrana3426@gmail.com>
2026-01-06 19:51:22 +0600

    feat: update radius in globals.css and add Modal and Dialog components

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
-'use client'
 import Skeleton from '@/components/shared/Skeleton'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-zinc-900">
-      <h1 className="text-4xl font-bold text-zinc-900 dark:text-zinc-100">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-zinc-900">
+      <h1 className="text-4xl font-bold">
         Welcome to Next.js! statter template💥💬
       </h1>
       <br />
       <div>
         <Skeleton className="m-4 h-30 w-48 rounded-md" />
+      </div>
+      <div className="bg-background text-foreground border-border border p-5">
+        <h1 className="text-xl">হ্যালো জুয়েল রানা!</h1>
+        <button className="bg-primary text-primary-foreground rounded-lg p-2">
+          ক্লিক করুন
+        </button>
       </div>
     </div>
   )
---
+++ components/shared/Modal.tsx
@@ -0,0 +1,45 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+
+interface ModalProps {
+  trigger?: React.ReactNode // মোডাল খোলার জন্য ট্রিগার এলিমেন্ট
+  title: string
+  description?: string
+  children: React.ReactNode
+  className?: string
+  open?: boolean
+  onOpenChange?: (open: boolean) => void
+}
+
+export function Modal({
+  trigger,
+  title,
+  description,
+  children,
+  className,
+  open,
+  onOpenChange,
+}: ModalProps) {
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      {/* যদি trigger প্রপস থাকে তবেই এটি দেখাবে */}
+      {trigger && <DialogTrigger asChild>{trigger}</DialogTrigger>}
+
+      <DialogContent className={className || 'sm:max-w-106.25'}>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          {description && <DialogDescription>{description}</DialogDescription>}
+        </DialogHeader>
+
+        {/* মেইন কন্টেন্ট এখানে রেন্ডার হবে */}
+        <div className="py-4">{children}</div>
+      </DialogContent>
+    </Dialog>
+  )
+}
